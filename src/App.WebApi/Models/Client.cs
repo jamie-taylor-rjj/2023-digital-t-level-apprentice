8eb3bb4fe9d859b919b66161010a6586f403e183
Jamie Taylor <seppydude@gmail.com>
2023-05-15 12:10:30 +0100

    Added basic Client model

---
+++ src/App.WebApi/Models/Client.cs
@@ -0,0 +1,12 @@
+﻿namespace App.WebApi.Models;
+
+public class Client
+{
+    public Client(int clientId, string clientName)
+    {
+        ClientId = clientId;
+        ClientName = clientName;
+    }
+    public int ClientId { get; set; }
+    public string ClientName { get; set; }
+}
