3beb4e0cac6de2665bdf2fc871c8cdf9447573d3
RobCode05 <05roberttuliao@gmail.com>
2023-06-02 14:37:24 +0100

    Finished validation

--- src/Invoice_GenUI/Invoice_GenUI/CreateClientWindow.xaml.cs
+++ src/Invoice_GenUI/Invoice_GenUI/CreateClientWindow.xaml.cs
@@ -3,59 +3,70 @@ using System.Windows;
 using System.Windows.Input;
 using Invoice_GenUI.ViewModels;
 
-namespace Invoice_GenUI
+namespace Invoice_GenUI;
+
+
+public partial class CreateClientWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for CreateClientWindow.xaml
-    /// </summary>
-    public partial class CreateClientWindow : Window
+    private readonly CreateClientViewModel _viewModel;
+    public CreateClientWindow(CreateClientViewModel viewModel)
     {
-        private readonly CreateClientViewModel _viewModel;
-        public CreateClientWindow(CreateClientViewModel viewModel)
+        _viewModel = viewModel;
+
+        InitializeComponent();
+
+        DataContext = _viewModel;
+    }
+    private bool HasText()
+    {
+        if (string.IsNullOrWhiteSpace(txt_clientName.Text) ||
+            string.IsNullOrWhiteSpace(txt_clientAddress.Text) ||
+            string.IsNullOrWhiteSpace(txt_clientContact.Text) ||
+            string.IsNullOrWhiteSpace(txt_clientEmail.Text))
         {
-            _viewModel = viewModel;
-
-            InitializeComponent();
-
-            DataContext = _viewModel;
+            return false;
         }
-        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        else
         {
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                this.DragMove();
-            }
+            return true;
         }
-
-        protected override void OnClosing(CancelEventArgs e)
+    }
+    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
         {
-            e.Cancel = true;
-            this.Hide();
+            this.DragMove();
         }
+    }
 
-        private void btn_createClient_Click(object sender, RoutedEventArgs e)
+    protected override void OnClosing(CancelEventArgs e)
+    {
+        e.Cancel = true;
+        this.Hide();
+    }
+
+    private void btn_createClient_Click(object sender, RoutedEventArgs e)
+    {
+        btn_createClient.IsEnabled = false;
+
+        if (string.IsNullOrWhiteSpace(txt_clientName.Text) ||
+            string.IsNullOrWhiteSpace(txt_clientAddress.Text) ||
+            string.IsNullOrWhiteSpace(txt_clientContact.Text) ||
+            string.IsNullOrWhiteSpace(txt_clientEmail.Text))
         {
-            btn_createClient.IsEnabled = false;
-
-            if (string.IsNullOrWhiteSpace(txt_clientName.Text) ||
-                string.IsNullOrWhiteSpace(txt_clientAddress.Text) ||
-                string.IsNullOrWhiteSpace(txt_clientContact.Text) ||
-                string.IsNullOrWhiteSpace(txt_clientEmail.Text))
-            {
-                MessageBox.Show("Invalid data");
-                btn_createClient.IsEnabled = true;
-            }
-            else
-            {
-
-                
-            }
-
+            MessageBox.Show("Invalid data");
             btn_createClient.IsEnabled = true;
+        }
+        else
+        {
 
             
         }
 
+        btn_createClient.IsEnabled = true;
+
+        
     }
 
+    
 }
