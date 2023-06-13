6ae29d9c3670fe2b4b65a639c2d91df2a40ac4e8
Jamie Taylor <jamie.taylor@rjj-software.co.uk>
2023-06-13 18:00:14 +0100

    Made logging better

--- src/Invoice_Gen.WebApi/Services/ClientService.cs
+++ src/Invoice_Gen.WebApi/Services/ClientService.cs
@@ -16,44 +16,53 @@ public class ClientService : IClientService
     }
     public List<ClientViewModel> GetClients()
     {
-        _logger.BeginScope("{ClientService} getting all clients", nameof(ClientService));
-        var all = _clientRepository.GetAll();
+        using (_logger.BeginScope("{ClientService} getting all clients", nameof(ClientService)))
+        {
+            var all = _clientRepository.GetAll();
 
-        _logger.LogInformation("Retrieved {Count} {ClientModel}", all.Count, nameof(Client));
+            _logger.LogInformation("Retrieved {Count} {ClientModel}", all.Count, nameof(Client));
 
-        _logger.LogInformation("Converting to List of {ClientNameViewModel} using {Mapper}", nameof(ClientViewModel), typeof(ClientNameViewModelMapper));
-        var returnData = all.Select(c => _clientViewModelMapper.Convert(c)).ToList();
+            _logger.LogInformation("Converting to List of {ClientNameViewModel} using {Mapper}",
+                nameof(ClientViewModel), typeof(ClientNameViewModelMapper));
+            var returnData = all.Select(c => _clientViewModelMapper.Convert(c)).ToList();
 
-        _logger.LogInformation("Returning {count} of {ClientNameViewModel} instances", returnData.Count(), nameof(ClientViewModel));
-        return returnData;
+            _logger.LogInformation("Returning {count} of {ClientNameViewModel} instances", returnData.Count(),
+                nameof(ClientViewModel));
+            return returnData;
+        }
     }
 
     public ClientViewModel? GetById(int id)
     {
-        _logger.BeginScope("{ClientService} getting client record for {ID}", nameof(ClientService), id);
+        using (_logger.BeginScope("{ClientService} getting client record for {ID}", nameof(ClientService), id))
+        {
+            var client = _clientRepository.GetAll().FirstOrDefault(f => f.ClientId == id);
 
-        var client = _clientRepository.GetAll().FirstOrDefault(f => f.ClientId == id);
-
-        _logger.LogInformation("Returning {ClientNameViewModel} for {ID}", nameof(ClientViewModel), id);
-        return client == null ? null : _clientViewModelMapper.Convert(client);
+            _logger.LogInformation("Returning {ClientNameViewModel} for {ID}", nameof(ClientViewModel), id);
+            return client == null ? null : _clientViewModelMapper.Convert(client);
+        }
     }
 
     public async Task<int> CreateNewClient(ClientCreationModel inputClient)
     {
-        _logger.BeginScope("{ClientService} creating new client record for {ClientName}", nameof(ClientService), inputClient.ClientName);
-        var response = await _clientRepository.Add(new Client
+        using (_logger.BeginScope("{ClientService} creating new client record for {ClientName}", nameof(ClientService),
+                   inputClient.ClientName))
         {
-            ClientAddress = inputClient.ClientAddress,
-            ClientName = inputClient.ClientName,
-            ContactEmail = inputClient.ContactEmail,
-            ContactName = inputClient.ContactName
-        });
-        _logger.LogInformation("Generated ID of new client is {ClientId}", response.ClientId);
-        return response.ClientId;
+            var response = await _clientRepository.Add(new Client
+            {
+                ClientAddress = inputClient.ClientAddress,
+                ClientName = inputClient.ClientName,
+                ContactEmail = inputClient.ContactEmail,
+                ContactName = inputClient.ContactName
+            });
+            _logger.LogInformation("Generated ID of new client is {ClientId}", response.ClientId);
+            return response.ClientId;
+        }
     }
 
     public async Task DeleteClient(int clientId)
     {
+        _logger.LogInformation("Deleting client with ID of {ClientId}", clientId);
         await _clientRepository.Delete(clientId);
     }
 }
