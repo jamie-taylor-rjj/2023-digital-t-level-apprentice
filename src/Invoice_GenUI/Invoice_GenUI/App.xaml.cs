7749c3f27f958d85e6a2dbba14ed67c1a87777e7
RobCode05 <05roberttuliao@gmail.com>
2023-06-18 23:36:48 +0100

    Created service collection

--- src/Invoice_GenUI/Invoice_GenUI/App.xaml.cs
+++ src/Invoice_GenUI/Invoice_GenUI/App.xaml.cs
@@ -1,13 +1,24 @@
 ﻿using System.Windows;
+using Invoice_GenUI.ViewModels;
+using Invoice_GenUI.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Invoice_GenUI
 {
     public partial class App : Application
     {
+        private readonly ServiceProvider _serviceProvider;
         public App()
         {
-           
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<MainWindow>();
 
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<HomeViewModel>();
+            services.AddTransient<InvoiceViewModel>();
+            
+
+            _serviceProvider = services.BuildServiceProvider();
         }
     }
 }
