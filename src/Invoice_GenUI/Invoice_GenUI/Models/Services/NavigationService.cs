778fe267ad26282b7b2404b7f3b85aae916d551a
RobCode05 <05roberttuliao@gmail.com>
2023-06-18 23:50:43 +0100

    Navigation service, current view property

---
+++ src/Invoice_GenUI/Invoice_GenUI/Models/Services/NavigationService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoice_GenUI.Models.Services
+{
+    public interface INavigationService
+    {
+        ViewModel CurrentView { get; }
+        
+    }
+    public class NavigationService : ViewModel, INavigationService
+    {
+       
+        private ViewModel _currentView;
+        public ViewModel CurrentView
+        {
+            get => _currentView;
+            private set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public NavigationService()
+        {
+            
+        }
+
+        
+
+    }
+}
