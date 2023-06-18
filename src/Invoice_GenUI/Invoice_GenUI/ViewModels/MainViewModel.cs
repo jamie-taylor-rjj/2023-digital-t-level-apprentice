fb8e976281fccc9088841d50c2f11a07bd374755
RobCode05 <05roberttuliao@gmail.com>
2023-06-19 00:13:38 +0100

    Injected navigation service

--- src/Invoice_GenUI/Invoice_GenUI/ViewModels/MainViewModel.cs
+++ src/Invoice_GenUI/Invoice_GenUI/ViewModels/MainViewModel.cs
@@ -1,9 +1,25 @@
 ﻿using Invoice_GenUI.Models;
+using Invoice_GenUI.Models.Services;
 
 namespace Invoice_GenUI.ViewModels
 {
     public partial class MainViewModel : ViewModel
     {
+        private INavigationService _navigation;
 
+        public INavigationService Navigation
+        {
+            get => _navigation;
+            set
+            {
+                _navigation = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel(INavigationService navService)
+        {
+            Navigation = navService;
+        }
     }
 }
