43f488d31f43feb7b87227369881e6c70cbf6201
RobCode05 <05roberttuliao@gmail.com>
2023-06-19 10:52:06 +0100

    Added the invoice UI

--- src/Invoice_GenUI/Invoice_GenUI/ViewModels/InvoiceViewModel.cs
+++ src/Invoice_GenUI/Invoice_GenUI/ViewModels/InvoiceViewModel.cs
@@ -1,9 +1,23 @@
-﻿using Invoice_GenUI.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Invoice_GenUI.Models;
+using Invoice_GenUI.Models.Services;
 
 namespace Invoice_GenUI.ViewModels
 {
     public partial class InvoiceViewModel : ViewModel
     {
+        [ObservableProperty]
+        private INavigationService _navigation;
 
+        public InvoiceViewModel(INavigationService navService)
+        {
+            Navigation = navService;
+        }
+        [RelayCommand]
+        public void GoBack()
+        {
+            Navigation.NavigateTo<HomeViewModel>();
+        }
     }
 }
