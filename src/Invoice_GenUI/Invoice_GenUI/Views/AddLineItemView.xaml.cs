baf63782ac78f39838ff30104aae19d2ea116cfd
RobCode05 <05roberttuliao@gmail.com>
2023-06-20 02:33:53 +0100

    Added line item view and its data template

---
+++ src/Invoice_GenUI/Invoice_GenUI/Views/AddLineItemView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Invoice_GenUI.Views
+{
+    /// <summary>
+    /// Interaction logic for AddLineItemView.xaml
+    /// </summary>
+    public partial class AddLineItemView : UserControl
+    {
+        public AddLineItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
