ba14f411d97465a245fcfef911a56e5424c10bad
RobCode05 <05roberttuliao@gmail.com>
2023-06-30 15:55:41 +0100

    Modified invoice services

--- src/Invoice_GenUI/Invoice_GenUI/Models/Services/InvoiceListService.cs
+++ src/Invoice_GenUI/Invoice_GenUI/Models/Services/InvoiceListService.cs
@@ -1,43 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Json;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Invoice_GenUI.Models.Services
 {
-    public class InvoiceListService : IInvoiceListService
+    public class InvoiceListService : BaseService, IInvoiceListService
     {
         public async Task<List<InvoiceModel>> GetInvoices()
         {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("https://2023-invoice-gen.azurewebsites.net/");
-
-                var response = await client.GetAsync("Invoice");
-
-                response.EnsureSuccessStatusCode();
-
-                return await response.Content.ReadFromJsonAsync<List<InvoiceModel>>() ?? new();
-            }
+            return await SendHttpRequest<List<InvoiceModel>>("Invoice") ?? new();
         }
         public async Task<bool> DeleteInvoice(int id)
         {
             bool result = false;
-            using (var client = new HttpClient())
+            using (var client = CreateHttpClient())
             {
-                client.BaseAddress = new Uri("https://2023-invoice-gen.azurewebsites.net/");
-
-                var respone = await client.DeleteAsync($"Invoice/{id}");
-
-                respone.EnsureSuccessStatusCode();
-
-                if (respone.IsSuccessStatusCode)
+                var response = await client.DeleteAsync($"Invoice/{id}");
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
                 {
                     result = true;
                 }
-                return result;
             }
+            return result;
         }
     }
 }
