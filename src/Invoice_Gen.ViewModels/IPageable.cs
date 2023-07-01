5fe2aa2058e560442fde2f2e1a08ef94b69307be
Jamie Taylor <jamie.taylor@rjj-software.co.uk>
2023-07-01 21:50:50 +0100

    Added docs explaining what IPageable.cs is

--- src/Invoice_Gen.ViewModels/IPageable.cs
+++ src/Invoice_Gen.ViewModels/IPageable.cs
@@ -1,5 +1,9 @@
 ﻿namespace Invoice_Gen.ViewModels;
 
+/// <summary>
+/// Used to lock the <see cref="PagedResponse{T}"/> down to specific types as
+/// we don't want to be able to create paged lists of arbitrary types 
+/// </summary>
 public interface IPageable
 {
 
